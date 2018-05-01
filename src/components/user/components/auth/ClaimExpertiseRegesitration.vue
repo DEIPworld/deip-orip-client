c8c21d3cf47e882a52d4b1134efe63ff4ddbfe5f
Nikolay Ignatiev <nikolay.ignatiev@deip.world>
2018-05-02 02:53:53 +0300

    hotfix

--- src/components/user/components/auth/ClaimExpertiseRegesitration.vue
+++ src/components/user/components/auth/ClaimExpertiseRegesitration.vue
@@ -43,7 +43,7 @@
 
 <script>
     export default {
-        name: 'EmailSendingRegesitration',
+        name: 'ClaimExpertiseRegesitration',
         data() {
             return {
                 popular: [
