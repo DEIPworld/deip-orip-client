224bc13209f6cd3e9c785103c35cabdf26da1c5b
Nikolay <ignatiev.ns@gmail.com>
2018-10-05 15:19:18 +0000

    Claim expertise templates

---
+++ src/components/claim-expertise/ClaimUserExpertisePage.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-container fluid fill-height class="pa-0 column-page">
+
+        <div class="content-column">
+            <div class="filling">
+                <claim-user-expertise-body></claim-user-expertise-body>
+            </div>
+        </div>
+
+        <v-card height="100%" class="sidebar">
+            <claim-user-expertise-sidebar></claim-user-expertise-sidebar>
+        </v-card>
+
+    </v-container>   
+</template>
+
+<script>
+    export default {
+        name: 'ClaimUserExpertisePage',
+        data() {
+            return {
+            }
+        },
+        methods: {
+
+        },
+        created() {
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+</style>
