2bb670b8900693c8d365d0758ee9774ba17489db
egortsaryk9 <egortsaryk@gmail.com>
2018-10-15 03:26:19 +0300

    Expertise claims http service

---
+++ src/services/http/expertiseClaims.js
@@ -0,0 +1,20 @@
+import http from './http'
+const apiUrl = process.env.DEIP_SERVER_URL;
+
+
+const service = {
+    getExpertiseClaimsByUser: (username) => {
+        return http.get(`/expertise-claims/user/${username}`, { baseURL: `${apiUrl}/api/` });
+    },
+    getExpertiseClaimsByDiscipline: (disciplineId) => {
+        return http.get(`/expertise-claims/discipline/${disciplineId}`, { baseURL: `${apiUrl}/api/` });
+    },
+    getExpertiseClaimsByUserAndDiscipline: (username, disciplineId) => {
+        return http.get(`/expertise-claims/user/${username}/discipline/${disciplineId}`, { baseURL: `${apiUrl}/api/` });
+    },
+    createExpertiseClaim: (username, disciplineId, coverLetter, publications) => {
+        return http.post(`/expertise-claims`, { username, disciplineId, coverLetter, publications }, { baseURL: `${apiUrl}/api/` });
+    }
+}
+
+export default service;
