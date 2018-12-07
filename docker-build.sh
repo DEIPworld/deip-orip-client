872f018c8780cf5ae6fe42b5f4d8ecad52ceb1d1
egortsaryk9 <egortsaryk@gmail.com>
2018-12-08 00:09:42 +0300

    docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 TAG=$(git log -1 --pretty=%h)
@@ -7,4 +6,10 @@ LATEST="latest"
 
 echo "Building deipworld/client image..."
 export IMAGE_NAME="deipworld/client:$TAG"
-npm install && npm run build && docker build -t=${IMAGE_NAME} . && docker push ${IMAGE_NAME}
+export LATEST_IMAGE_NAME="deipworld/client:$LATEST"
+npm install && 
+npm run build && 
+docker build -t=${IMAGE_NAME} . && 
+docker tag ${IMAGE_NAME} ${LATEST_IMAGE_NAME} && 
+docker push ${IMAGE_NAME} && 
+docker push ${LATEST_IMAGE_NAME}
