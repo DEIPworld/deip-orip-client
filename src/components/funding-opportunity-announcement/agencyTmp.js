c9f1d4a7bec7c74606f32eadeec92e4f14550dd2
Nikolay Ignatiev <nikolay.ignatiev@deip.world>
2019-01-03 16:30:05 +0300

    agency name on creation is disabled

---
+++ src/components/funding-opportunity-announcement/agencyTmp.js
@@ -0,0 +1,383 @@
+export default {
+    "_id" : "nsf",
+    "name" : "National Science Foundation",
+    "shortName" : "NSF",
+    "description" : "Manufacturers, regulators and consumers look to NSF International for the development of public health standards and certification programs that help protect the world’s food, water, consumer products and environment. Our mission is to protect and improve global human health. As an independent, accredited organization, our standards group facilitates the development of standards, and our service groups test and certify products and systems. We also provide auditing and risk management solutions for public health and the environment. Founded in 1944 as the National Sanitation Foundation, we changed our name to NSF International in 1990 as we expanded our services beyond sanitation and into global markets. The letters in our organization's name do not represent any specific words today.",
+    "logo" : "nsf.png",
+    "email" : "grantsgovsupport@nsf.gov",
+    "researchAreas" : [ 
+        {
+            "title" : "Biological Sciences (BIO)",
+            "abbreviation" : "BIO",
+            "disciplines" : [ 
+                3
+            ],
+            "subAreas" : [ 
+                {
+                    "title" : "Biological Infrastructure (DBI)",
+                    "abbreviation" : "DBI",
+                    "disciplines" : [ 
+                        3
+                    ]
+                }, 
+                {
+                    "title" : "Environmental Biology (DEB)",
+                    "abbreviation" : "DEB",
+                    "disciplines" : [ 
+                        3
+                    ]
+                }, 
+                {
+                    "title" : "Emerging Frontiers (EF)",
+                    "abbreviation" : "EF",
+                    "disciplines" : [ 
+                        3
+                    ]
+                }, 
+                {
+                    "title" : "Integrative Organismal Systems (IOS)",
+                    "abbreviation" : "IOS",
+                    "disciplines" : [ 
+                        3
+                    ]
+                }, 
+                {
+                    "title" : "Molecular and Cellular Biosciences (MCB)",
+                    "abbreviation" : "MCB",
+                    "disciplines" : [ 
+                        3
+                    ]
+                }
+            ]
+        }, 
+        {
+            "title" : "Computer and Information Science and Engineering (CISE)",
+            "abbreviation" : "CISE",
+            "disciplines" : [ 
+                8, 
+                138, 
+                135, 
+                140, 
+                145, 
+                147
+            ],
+            "subAreas" : [ 
+                {
+                    "title" : "Computing and Communication Foundations (CCF)",
+                    "abbreviation" : "CCF",
+                    "disciplines" : [ 
+                        8, 
+                        145, 
+                        147, 
+                        151
+                    ]
+                }, 
+                {
+                    "title" : "Computer and Network Systems (CNS)",
+                    "abbreviation" : "CNS",
+                    "disciplines" : [ 
+                        8, 
+                        138
+                    ]
+                }, 
+                {
+                    "title" : "Information & Intelligent Systems (IIS)",
+                    "abbreviation" : "IIS",
+                    "disciplines" : [ 
+                        8, 
+                        140, 
+                        135
+                    ]
+                }, 
+                {
+                    "title" : "Advanced Cyberinfrastructure (OAC)",
+                    "abbreviation" : "OAC",
+                    "disciplines" : [ 
+                        8, 
+                        151
+                    ]
+                }
+            ]
+        }, 
+        {
+            "title" : "Education and Human Resources (EHR)",
+            "abbreviation" : "EHR",
+            "disciplines" : [ 
+                2
+            ],
+            "subAreas" : [ 
+                {
+                    "title" : "Graduate Education (DGE)",
+                    "abbreviation" : "DGE",
+                    "disciplines" : [ 
+                        2
+                    ]
+                }, 
+                {
+                    "title" : "Undergraduate Education (DUE)",
+                    "abbreviation" : "DUE",
+                    "disciplines" : [ 
+                        2
+                    ]
+                }, 
+                {
+                    "title" : "Human Resource Development (HRD)",
+                    "abbreviation" : "HRD",
+                    "disciplines" : [ 
+                        2
+                    ]
+                }
+            ]
+        }, 
+        {
+            "title" : "Engineering (ENG)",
+            "abbreviation" : "ENG",
+            "disciplines" : [ 
+                4
+            ],
+            "subAreas" : [ 
+                {
+                    "title" : "Civil, Mechanical and Manufacturing Innovation (CMMI)",
+                    "abbreviation" : "CMMI",
+                    "disciplines" : [ 
+                        4
+                    ]
+                }, 
+                {
+                    "title" : "Electrical, Communications and Cyber Systems (ECCS)",
+                    "abbreviation" : "ECCS",
+                    "disciplines" : [ 
+                        4
+                    ]
+                }, 
+                {
+                    "title" : "Engineering Education and Centers (EEC)",
+                    "abbreviation" : "EEC",
+                    "disciplines" : [ 
+                        4
+                    ]
+                }, 
+                {
+                    "title" : "Emerging Frontiers and Multidisciplinary Activities (EFMA)",
+                    "abbreviation" : "EFMA",
+                    "disciplines" : [ 
+                        4
+                    ]
+                }, 
+                {
+                    "title" : "Industrial Innovation and Partnerships (IIP)",
+                    "abbreviation" : "IIP",
+                    "disciplines" : [ 
+                        4
+                    ]
+                }
+            ]
+        }, 
+        {
+            "title" : "Environmental Research and Education (ERE)",
+            "abbreviation" : "ERE",
+            "disciplines" : [ 
+                106
+            ],
+            "subAreas" : [ 
+                {
+                    "title" : "Environmental Research and Education (ERE)",
+                    "abbreviation" : "ERE",
+                    "disciplines" : [ 
+                        106
+                    ]
+                }
+            ]
+        }, 
+        {
+            "title" : "Geosciences (GEO)",
+            "abbreviation" : "GEO",
+            "disciplines" : [ 
+                6, 
+                109, 
+                113, 
+                112
+            ],
+            "subAreas" : [ 
+                {
+                    "title" : "Atmospheric and Geospace Sciences (AGS)",
+                    "abbreviation" : "AGS",
+                    "disciplines" : [ 
+                        6, 
+                        112
+                    ]
+                }, 
+                {
+                    "title" : "Earth Sciences (EAR)",
+                    "abbreviation" : "EAR",
+                    "disciplines" : [ 
+                        6, 
+                        109
+                    ]
+                }, 
+                {
+                    "title" : "Ocean Sciences (OCE)",
+                    "abbreviation" : "OCE",
+                    "disciplines" : [ 
+                        6, 
+                        113
+                    ]
+                }, 
+                {
+                    "title" : "Polar Programs (OPP)",
+                    "abbreviation" : "OPP",
+                    "disciplines" : [ 
+                        6, 
+                        119
+                    ]
+                }
+            ]
+        }, 
+        {
+            "title" : "Integrative Activities (OIA)",
+            "abbreviation" : "OIA",
+            "disciplines" : [ 
+                32
+            ],
+            "subAreas" : [ 
+                {
+                    "title" : "Integrative Activities (IA)",
+                    "disciplines" : [ 
+                        32
+                    ],
+                    "abbreviation" : "IA"
+                }, 
+                {
+                    "title" : "Established Program to Stimulate Competitive Research (EPSCoR)",
+                    "disciplines" : [ 
+                        32
+                    ],
+                    "abbreviation" : "EPSCoR"
+                }, 
+                {
+                    "title" : "Evaluation and Assessment Capability (EAC)",
+                    "abbreviation" : "EAC",
+                    "disciplines" : [ 
+                        32
+                    ]
+                }
+            ]
+        }, 
+        {
+            "title" : "International Science and Engineering (OISE)",
+            "abbreviation" : "OISE",
+            "disciplines" : [ 
+                72
+            ],
+            "subAreas" : [ 
+                {
+                    "title" : "International Science and Engineering (OISE)",
+                    "abbreviation" : "OISE",
+                    "disciplines" : [ 
+                        72
+                    ]
+                }
+            ]
+        }, 
+        {
+            "title" : "Mathematical and Physical Sciences (MPS)",
+            "abbreviation" : "MPS",
+            "disciplines" : [ 
+                10, 
+                5, 
+                4, 
+                122, 
+                80, 
+                81
+            ],
+            "subAreas" : [ 
+                {
+                    "title" : "Astronomical Sciences (AST)",
+                    "abbreviation" : "AST",
+                    "disciplines" : [ 
+                        123
+                    ]
+                }, 
+                {
+                    "title" : "Chemistry (CHE)",
+                    "abbreviation" : "CHE",
+                    "disciplines" : [ 
+                        5
+                    ]
+                }, 
+                {
+                    "title" : "Materials Research (DMR)",
+                    "abbreviation" : "DMR",
+                    "disciplines" : [ 
+                        80, 
+                        81
+                    ]
+                }, 
+                {
+                    "title" : "Mathematical Sciences (DMS)",
+                    "abbreviation" : "DMS",
+                    "disciplines" : [ 
+                        10
+                    ]
+                }, 
+                {
+                    "title" : "Physics (PHY)",
+                    "abbreviation" : "PHY",
+                    "disciplines" : [ 
+                        4
+                    ]
+                }, 
+                {
+                    "title" : "Office of Multidisciplinary Activities (OMA)",
+                    "abbreviation" : "OMA",
+                    "disciplines" : [ 
+                        80, 
+                        81
+                    ]
+                }
+            ]
+        }, 
+        {
+            "title" : "Social, Behavioral and Economic Sciences (SBE)",
+            "abbreviation" : "SBE",
+            "disciplines" : [ 
+                1, 
+                39
+            ],
+            "subAreas" : [ 
+                {
+                    "title" : "Behavioral and Cognitive Sciences (BCS)",
+                    "abbreviation" : "BCS",
+                    "disciplines" : [ 
+                        1, 
+                        39
+                    ]
+                }, 
+                {
+                    "title" : "National Center for Science and Engineering Statistics (NCSES)",
+                    "abbreviation" : "NCSES",
+                    "disciplines" : [ 
+                        1, 
+                        39
+                    ]
+                }, 
+                {
+                    "title" : "Social and Economic Sciences (SES)",
+                    "abbreviation" : "SES",
+                    "disciplines" : [ 
+                        1,
+                        39
+                    ]
+                }, 
+                {
+                    "title" : "SBE Office of Multidisciplinary Activities (SMA)",
+                    "abbreviation" : "SMA",
+                    "disciplines" : [ 
+                        1, 
+                        39
+                    ]
+                }
+            ]
+        }
+    ]
+};
