c41fc6dab242f8272b8853eb7423b1a1fe756f2c
egortsaryk9 <egortsaryk@gmail.com>
2019-11-15 17:19:39 +0300

    Horizontal top menu

--- src/components/layout/components/Toolbar.vue
+++ src/components/layout/components/Toolbar.vue
@@ -4,89 +4,44 @@
             <global-loader></global-loader>
         </div>
 
-        <v-toolbar app fixed clipped-left color="black" dark class="deip-toolbar">
+        <v-toolbar color="black">
+            <v-toolbar-side-icon class="mx-2">
+                <router-link class="app-title" :to="{ name: 'Default' }" tag="img" src="./static/logo.svg"></router-link>
+            </v-toolbar-side-icon>
             <v-toolbar-title>
-                <router-link v-if="tenant" :to="{ name: 'Default' }">
-                    <v-avatar size="60px">
-                        <img :src="tenant | agencyLogoSrc(160, 160, false)" />
-                    </v-avatar>
-                </router-link>
-                <router-link v-else class="app-title" :to="{ name: 'Default' }" tag="img" src="./static/logo.svg"></router-link>
+
             </v-toolbar-title>
-
             <v-spacer></v-spacer>
+            <v-toolbar-items v-if="isLoggedIn()" class="hidden-sm-and-down">
+                <v-btn :to="{ name: 'ResearchFeed' }" color="white" flat class="ma-0">Feed</v-btn>
+                <v-btn :to="{ name: 'Dashboard' }"  color="white" flat class="ma-0">Dashboard</v-btn>
+                <v-btn :to="{ name: 'InvestorDashboard' }" color="white" flat class="ma-0">Portfolio</v-btn>
+                <notifications-list :notifications="user.notifications"></notifications-list>
+                <v-menu bottom left offset-y>
+                    <v-btn fab flat icon class="ma-0" slot="activator">
+                        <v-avatar size="32px">
+                            <img v-if="user.profile" v-bind:src="user.profile.avatar | avatarSrc(2 * 32, 2 * 32, false)" />
+                            <v-gravatar v-if="!user.profile && user.account" :title="user.username" :email="user.username + '@deip.world'" />
+                        </v-avatar>
+                    </v-btn>
+                    <v-list class="dropdown-list" dark dense>
+                        <v-list-tile :to="{ name: 'UserDetails', params: { account_name: user.username } }">
+                            <v-list-tile-title>Profile</v-list-tile-title>
+                        </v-list-tile>
+                        <v-list-tile :to="{ name: 'UserWallet', params: { account_name: user.username } }">
+                            <v-list-tile-title>Wallet</v-list-tile-title>
+                        </v-list-tile>
+                        <v-divider></v-divider>
+                        <v-list-tile @click="signOut()">
+                            <v-list-tile-title>Sign Out</v-list-tile-title>
+                        </v-list-tile>
+                    </v-list>
+                </v-menu>
+            </v-toolbar-items>
 
-            <v-btn v-if="isLoggedIn()" :to="{name: 'ResearchFeed'}" icon large class="ma-0">
-                <v-icon size="32px" color="grey lighten-1">search</v-icon>
-            </v-btn>
-
-            <notifications-list v-if="isLoggedIn()" :notifications="user.notifications"></notifications-list>
-
-            <v-menu v-if="isLoggedIn()" bottom left offset-y>
-                <v-btn fab flat icon class="ma-0" slot="activator">
-                    <v-avatar size="32px">
-                        <img v-if="user.profile" v-bind:src="user.profile.avatar | avatarSrc(2 * 32, 2 * 32, false)" />
-                        <v-gravatar v-if="!user.profile && user.account" :title="user.username" :email="user.username + '@deip.world'" />
-                    </v-avatar>
-                </v-btn>
-
-                <v-list class="dropdown-list" dark dense>
-                    <v-list-tile :to="{ name: 'UserDetails', params: { account_name: user.username } }">
-                        <v-list-tile-title>Profile</v-list-tile-title>
-                    </v-list-tile>
-
-                    <!-- temporary commented for the NSF demo -->
-                    
-                    <v-list-tile :to="{ name: 'UserWallet' }">
-                        <v-list-tile-title>Wallet</v-list-tile-title>
-                    </v-list-tile>
-
-                    <v-list-tile :to="{ name: 'InvestorDashboard' }">
-                        <v-list-tile-title>Portfolio</v-list-tile-title>
-                    </v-list-tile>
-
-                    <v-list-tile :to="{ name: 'Dashboard' }">
-                        <v-list-tile-title>Dashboard</v-list-tile-title>
-                    </v-list-tile>
-
-                    <!-- <v-list-tile :to="{ name: 'ClaimUserExpertiseList' }">
-                        <v-list-tile-title>Expertise Committee</v-list-tile-title>
-                    </v-list-tile> -->
-
-                    <!-- <v-list-tile :to="{ name: 'VotingForBlockProducers' }">
-                        <v-list-tile-title>Voting for block producers</v-list-tile-title>
-                    </v-list-tile> -->
-                    
-                    
-                <!-- <v-list-tile v-if="user" :to="{ name: 'CreateDisciplineGrant' }">
-                        <v-list-tile-title>Create Grant</v-list-tile-title>
-                    </v-list-tile> -->
-
-                   <!-- <v-list-tile :to="{ name: 'investor-dashboard' }">
-                        <v-list-tile-title>Investor dashboard</v-list-tile-title>
-                    </v-list-tile> -->
-
-                    <!-- <v-list-tile v-if="isGrantor" :to="{ name: 'CreateFundingOpportunityAnnouncement' }">
-                        <v-list-tile-title>Create Funding Opportunity</v-list-tile-title>
-                    </v-list-tile> -->
-
-                    <v-divider></v-divider>
-
-                    <v-list-tile @click="signOut()">
-                        <v-list-tile-title>Sign Out</v-list-tile-title>
-                    </v-list-tile>
-                </v-list>
-            </v-menu>
-
-            <div v-if="isLoggedIn() && !isGrantor && !isOfficer" class="mx-3" style="width: 180px">
-                <v-btn dark round color="primary" class="full-width ma-0" :to="{ name: 'CreateResearch' }">
-                    <div class="legacy-col-grow">Create Project</div>
-                    <v-icon dark small class="ml-2">add</v-icon>
-                </v-btn>
-            </div>
-
-            <v-toolbar-items v-if="!isLoggedIn()">
-                <v-btn flat :to="{ name: 'SignUp' }">Sign In</v-btn>
+            <v-toolbar-items v-else>
+                <v-btn color="white" flat :to="{ name: 'SignIn' }">Sign In</v-btn>
+                <!-- <v-btn color="white" flat :to="{ name: 'SignUp' }">Sign Up</v-btn> -->
             </v-toolbar-items>
         </v-toolbar>
     </div>
