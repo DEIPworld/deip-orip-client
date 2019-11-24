7557363f9a9ee745ccee11d63312bc37b48b2de7
egortsaryk9 <egortsaryk@gmail.com>
2019-11-24 03:25:11 +0300

    SAFT dialog

--- src/components/research-details/ResearchDetails.vue
+++ src/components/research-details/ResearchDetails.vue
@@ -154,18 +154,23 @@
                     block
                   >Invest</v-btn>
                 </v-layout>
-                <confirm-action-dialog
-                  :meta="investmentConfirmDialog"
-                  :width="500"
-                  confirm-text="Agree"
-                  cancel-text="Disagree"
-                  title="SAFT Text"
-                  text="Miusov, as a man man of breeding and deilcacy, could not but feel some inwrd qualms, when he reached the Father Superior's with Ivan: he felt ashamed of havin lost his temper. He felt that he ought to have disdaimed that despicable wretch, Fyodor Pavlovitch, too much to have been upset by him in Father Zossima's cell, and so to have forgotten himself. Teh monks were not to blame, in any case, he reflceted, on the steps. And if theyre decent people here (and the Father Superior, I understand, is a nobleman) why not be friendly and courteous withthem? I wont argue, Ill fall in with everything, Ill win them by politness, and show them that Ive nothing to do with that Aesop, thta buffoon, that Pierrot, and have merely been takken in over this affair, just as they have. He determined to drop his litigation with the monastry, and relinguish his claims to the wood-cuting and fishery rihgts at once. He was the more ready to do this becuase the rights had becom much less valuable, and he had indeed the vaguest idea where the wood and river in quedtion were. These excellant intentions were strengthed when he enterd the Father Superiors diniing-room, though, stricttly speakin, it was not a dining-room, for the Father Superior had only two rooms alltogether; they were, however, much larger and more comfortable than Father Zossimas. But tehre was was no great luxury about the furnishng of these rooms eithar. The furniture was of mohogany, covered with leather, in the old-fashionned style of 1820 the floor was not even stained, but evreything was shining with cleanlyness, and there were many chioce flowers in the windows; the most sumptuous thing in the room at the moment was, of course, the beatifuly decorated table. The cloth was clean, the service shone; there were three kinds of well-baked bread, two bottles of wine, two of excellent mead, and a large glass jug of kvas -- both the latter made in the monastery, and famous in the neigborhood. There was no vodka. Rakitin related afterwards that there were five dishes: fish-suop made of sterlets, served with little fish paties; then boiled fish served in a spesial way; then salmon cutlets, ice pudding and compote, and finally, blanc-mange. Rakitin found out about all these good things, for he could not resist peeping into the kitchen, where he already had a footing. He had a footting everywhere, and got informaiton about everything. He was of an uneasy and envious temper. He was well aware of his own considerable abilities, and nervously exaggerated them in his self-conceit. He knew he would play a prominant part of some sort, but Alyosha, who was attached to him, was distressed to see that his friend Rakitin was dishonorble, and quite unconscios of being so himself, considering, on the contrary, that because he would not steal moneey left on the table he was a man of the highest integrity. Neither Alyosha nor anyone else could have infleunced him in that. Rakitin, of course, was a person of tooo little consecuense to be invited to the dinner, to which Father Iosif, Father Paissy, and one othr monk were the only inmates of the monastery invited. They were alraedy waiting when Miusov, Kalganov, and Ivan arrived. The other guest, Maximov, stood a little aside, waiting also. The Father Superior stepped into the middle of the room to receive his guests. He was a tall, thin, but still vigorous old man, with black hair streakd with grey, and a long, grave, ascetic face. He bowed to his guests in silence. But this time they approaced to receive his blessing. Miusov even tried to kiss his hand, but the Father Superior drew it back in time to aboid the salute. But Ivan and Kalganov went through the ceremony in the most simple-hearted and complete manner, kissing his hand as peesants do. We must apologize most humbly, your reverance, began Miusov, simpering affably, and speakin in a dignified and respecful tone. Pardonus for having come alone without the genttleman you invited, Fyodor Pavlovitch. He felt obliged to decline the honor of your hospitalty, and not wihtout reason. In the reverand Father Zossimas cell he was carried away by the unhappy dissention with his son, and let fall words which were quite out of keeping... in fact, quite unseamly... asyour reverance is, no doubt, already aware. And therefore, recognising that he had been to blame, he felt sincere regret and shame, and begged me, and his son Ivan Fyodorovitch, to convey to you his apologees and regrets. In brief, he hopes and desires to make amends later. He asks your blessinq, and begs you to forget what has takn place."
-                  @confirmed="contributeToTokenSale($event); investmentConfirmDialog.isShown = false;" 
-                  @canceled="investmentConfirmDialog.isShown = false">
-                </confirm-action-dialog>
+                <v-dialog v-model="investmentConfirmDialog.isShown" persistent max-width="800px">
+                  <v-card class="px-4 py-2">
+                    <v-card-title>
+                      <span class="headline bold">SAFT (Simple Agreement for Future Tokens)</span>
+                    </v-card-title>
+                    <v-card-text style="height: 50vh">
+                      <iframe height="100%" width="100%" :src="'./../../../static/form-of-SAFT-for-token-pre-sale.pdf'"></iframe>
+                    </v-card-text>
+                    <v-card-actions class="pa-0">
+                      <v-layout column>
+                        <v-btn class="mx-0 pa-0 my-2" color="primary" @click="agreeSaft()">Agree</v-btn>
+                        <v-btn class="mx-0 pa-0" color="primary" flat @click="disagreeSaft()">Disagree</v-btn>
+                      </v-layout>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
               </v-layout>
-
             </v-flex>
           </v-layout>
           <v-layout v-if="investors.length || isActiveTokenSale" class="my-5">
@@ -1405,6 +1410,17 @@
         }
 
         this.isExpertsLoading = false;
+      },
+
+      agreeSaft() {
+        this.investmentConfirmDialog.isShown = false;
+        setTimeout(() => {
+          this.contributeToTokenSale();
+        }, 100);
+      },
+
+      disagreeSaft() {
+        this.investmentConfirmDialog.isShown = false;
       },
 
       getContentType,
