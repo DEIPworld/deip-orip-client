ddcaba8e5a9c969bc1ca8dbeba85bc578601efab
egortsaryk9 <egortsaryk@gmail.com>
2019-11-26 23:50:51 +0300

    docker-build

--- docker-build.sh
+++ docker-build.sh
@@ -3,10 +3,12 @@ set -e
 
 TAG=$(git log -1 --pretty=%h)
 LATEST="latest"
+ORG="deipworld"
+IMAGE_PREFIX="$1"
 
-echo "Building deipworld/spb-demo-web-client image..."
-export IMAGE_NAME="deipworld/spb-demo-web-client:$TAG"
-export LATEST_IMAGE_NAME="deipworld/spb-demo-web-client:$LATEST"
+echo "Building $ORG/$IMAGE_PREFIX-web-client image..."
+export IMAGE_NAME="$ORG/$IMAGE_PREFIX-web-client:$TAG"
+export LATEST_IMAGE_NAME="$ORG/$IMAGE_PREFIX-web-client:$LATEST"
 
 docker build -t=${IMAGE_NAME} . && 
 docker tag ${IMAGE_NAME} ${LATEST_IMAGE_NAME} && 
