9b366a7bff3b587a916e633a19ba5e26013b2f61
Dmitry Nechay <dmitry.nechay@deip.world>
2019-12-16 14:07:59 +0300

    Restricted access to research content. Ability to request access

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -8,6 +8,9 @@ Vue.component('state-research-list', StateResearchList);
 import ConfirmActionDialog from './components/ConfirmActionDialog';
 Vue.component('confirm-action-dialog', ConfirmActionDialog);
 
+import RequestContentDialog from './components/RequestContentDialog';
+Vue.component('request-content-dialog', RequestContentDialog);
+
 import DatetimePicker from './components/DatetimePicker';
 Vue.component('datetime-picker', DatetimePicker);
 
