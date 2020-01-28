05530915a0e304a2cd54394ebf172a3c53fdc9b7
Dmitry Nechay <dmitry.nechay@deip.world>
2020-01-28 16:40:29 +0300

    Show latest action in history first

--- src/components/user-details/AccountExpertiseDetails.vue
+++ src/components/user-details/AccountExpertiseDetails.vue
@@ -197,7 +197,8 @@
         const { page, rowsPerPage: perPage } = this.historyTable.pagination;
         this.historyTable.loading = true;
         const detailsPromises = [];
-        const pageItems = this.history.slice((page - 1) * perPage, page * perPage);
+        const pageItems = this.history.reverse()
+          .slice((page - 1) * perPage, page * perPage);
         pageItems.forEach((pageItem, index) => {
           let detailsPromise;
           switch (pageItem.action) {
