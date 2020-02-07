0173c8622395cf6bbb58bef5632e07c727ede088
Egor Anikey <egor.anikey@deip.world>
2020-02-07 15:40:24 +0300

    small fix user settings

--- src/components/user-settings/UserSettings.vue
+++ src/components/user-settings/UserSettings.vue
@@ -1,35 +1,22 @@
 <template>
   <base-page-layout>
-    <user-settings-body slot="content"></User-settings-body>
+    <user-settings-body slot="content"></user-settings-body>
   </base-page-layout>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import deipRpc from '@deip/deip-oa-rpc-client';
-import moment from 'moment';
-
 export default {
   name: "UserSettings",
-  
-  computed: {
-    ...mapGetters({
-      user: "auth/user"
-    }),
-  },
+
   data() {
-    return {
-    };
+    return {};
   },
 
-  methods: {
-  },
+  methods: {},
 
-  created() {
-  }
+  created() {}
 };
 </script>
 
 <style lang="less" scoped>
-
 </style>
