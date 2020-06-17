d5e3897b022da75dda83cb71604d381eec5539f6
egortsaryk9 <egortsaryk@gmail.com>
2020-06-17 18:52:10 +0300

    Discipline ids

--- src/components/common/disciplines/DisciplineTree.js
+++ src/components/common/disciplines/DisciplineTree.js
@@ -4,46 +4,46 @@ export default {
   "path": "",
   "children": {
 
-    bio_products: {
-      label: 'Bio Products',
-      id: 1,
-      path: 'bio_products',
-      children: {}
+    "bio_products": {
+      "label": 'Bio Products',
+      "id": "56dba1440c40847fb1fedbee3cfef524081ee313",
+      "path": 'bio_products',
+      "children": {}
     },
 
-    bio_energy: {
-      label: 'Bio Energy',
-      id: 2,
-      path: 'bio_energy',
-      children: {},
+    "bio_energy": {
+      "label": 'Bio Energy',
+      "id": "1f5dc208490c6f4a8ec86b9dc12c4c1a9a8c420a",
+      "path": 'bio_energy',
+      "children": {},
     },
 
-    bio_food: {
-      label: 'Bio Food',
-      id: 3,
-      path: 'bio_food',
-      children: {},
+    "bio_food": {
+      "label": 'Bio Food',
+      "id": "f3d0968f8d47ffbca23352abcb90aadb345a55d1",
+      "path": 'bio_food',
+      "children": {},
     },
 
-    bio_chemical: {
-      label: 'Bio Chemical',
-      id: 4,
-      path: 'bio_chemical',
-      children: {}
+    "bio_chemical": {
+      "label": 'Bio Chemical',
+      "id": "b8ea1adb762f41fe10f193c58bca836b94fb0021",
+      "path": 'bio_chemical',
+      "children": {}
     },
 
-    bio_fiber: {
-      label: 'Bio Fiber',
-      id: 5,
-      path: 'bio_fiber',
-      children: {}
+    "bio_fiber": {
+      "label": 'Bio Fiber',
+      "id": "c4545e9398409ec03c6c2eae25c685ad323f2e60",
+      "path": 'bio_fiber',
+      "children": {}
     },
 
-    bio_mechanical: {
-      label: 'Bio Mechanical',
-      id: 6,
-      path: 'bio_mechanical',
-      children: {},
+    "bio_mechanical": {
+      "label": 'Bio Mechanical',
+      "id": "369a26e132a69db02a613a59fc33127f6652dd1d",
+      "path": 'bio_mechanical',
+      "children": {},
     }
   }
 }
