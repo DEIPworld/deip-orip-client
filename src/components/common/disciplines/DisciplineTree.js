946752ba20c8824dfa3272b088eb5504613d4256
egortsaryk9 <egortsaryk@gmail.com>
2021-01-05 03:21:45 +0100

    Disciplines list

--- src/components/common/disciplines/DisciplineTree.js
+++ src/components/common/disciplines/DisciplineTree.js
@@ -3,47 +3,71 @@ export default {
   id: '6c4bb3bcf1a88e3b51de88576d592f1f980c5bbb',
   path: '',
   children: {
-    biology: {
-      label: 'Biology',
-      id: '7c3d37cbfea2513a7e03e674448bbeee8ae3d862',
-      path: 'biology',
+    agriculture: {
+      label: 'Agriculture',
+      id: '18c642e8ac45d876f9aadf68ad021a33f4abd7f8',
+      path: 'agriculture',
       children: {}
     },
-    physics: {
-      label: 'Physics',
-      id: '9f0224709d86e02b9625b5ebf2786b80ba6bed17',
-      path: 'physics',
+    biotech: {
+      label: 'Biotech',
+      id: '112e8a2c5e4a9b417c6f1eb60f8b3c48288a7067',
+      path: 'biotech',
       children: {}
     },
-    chemistry: {
-      label: 'Chemistry',
-      id: '6a8b20f002a7dedf7b873dbc86e0b0051d4fa898',
-      path: 'chemistry',
+    chemistry_and_nanotech: {
+      label: 'Chemistry and Nanotech',
+      id: '36c62562adb44a1d491bb784bb12ff5980c42821',
+      path: 'chemistry_and_nanotech',
       children: {}
     },
-    earth_sciences: {
-      label: 'Earth sciences',
-      id: 'a47bf84ac30d0843accb737d5924434ef3ed0517',
-      path: 'earth_sciences',
+    cs_and_commtech: {
+      label: 'CS and Commtech',
+      id: '2cc618dfd6a0ed976af56a5a56a3edec5f9cf6ec',
+      path: 'cs_and_commtech',
       children: {}
     },
-    space_sciences: {
-      label: 'Space sciences',
-      id: '8e2a3711649993a87848337b9b401dcf64425e2d',
-      path: 'space_sciences',
+    ee_and_electro_optics: {
+      label: 'EE and Electro-optics',
+      id: '98cbdeea686073037e3b1452876d130086017fec',
+      path: 'ee_and_electro_optics',
       children: {}
     },
-    medicine_and_health: {
-      label: 'Medicine and health',
-      id: '721e75eb0535e152669b0c3fbbb9e21675483553',
-      path: 'medicine_and_health',
+    environmental_and_cleantech: {
+      label: 'Environmental and Cleantech',
+      id: '3abaef9b0f855c7a896a3e8e34e3622647e632ec',
+      path: 'environmental_and_cleantech',
       children: {}
     },
-    aquaculture: {
-      label: 'Aquaculture',
-      id: '2519ef55e1b69f1a7e13275e3273950cce7e26a8',
-      path: 'aquaculture',
+    hls: {
+      label: 'HLS',
+      id: '56e439d68f427ffd7308f07a63502fa9e71576f3',
+      path: 'hls',
       children: {}
-    }
+    },
+    mechanical_engineering: {
+      label: 'Mechanical Engineering',
+      id: '7b2675176600d1093987220c04e72ef0619f4ee8',
+      path: 'mechanical_engineering',
+      children: {}
+    },
+    medical_devices_and_physiotherapy: {
+      label: 'Medical Devices and Physiotherapy',
+      id: '526b4f76d045f0da00ed142ff3e66c2f6cf6d1d5',
+      path: 'medical_devices_and_physiotherapy',
+      children: {}
+    },
+    pharma: {
+      label: 'Pharma',
+      id: '7387154264560ae0a9e496b34e1ad53a50caf651',
+      path: 'pharma',
+      children: {}
+    },
+    physical_and_material_science: {
+      label: 'Physical and Material Science',
+      id: 'e43f1fe9194a3ee3dde930eabc3f53513beb0d88',
+      path: 'physical_and_material_science',
+      children: {}
+    },
   }
 };
