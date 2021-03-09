c8b4dc7678718b10b07695ee26b37cfa5ac9fc17
egortsaryk9 <egortsaryk@gmail.com>
2021-03-09 21:41:45 +0100

    attributeSet default value fixed

--- src/components/Attributes/_mixins/set/attributeSet.js
+++ src/components/Attributes/_mixins/set/attributeSet.js
@@ -57,7 +57,7 @@ export const attributeSet = {
   },
 
   created() {
-    if (!this.value && this.attribute.defaultValue !== null) {
+    if (this.value == null && this.attribute.defaultValue !== null) {
       this.internalValue = this.attribute.defaultValue;
     }
   },
